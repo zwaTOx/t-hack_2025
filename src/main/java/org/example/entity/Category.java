34b96713a4ac9218a26f27290dbd22b928f8e647
Vladislav <xelliks666@gmail.com>
2025-06-16 16:53:56 +0700

    Added new categories

---
+++ src/main/java/org/example/entity/Category.java
@@ -0,0 +1,20 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "color")
+    private String color;
+    @Column(name = "description")
+    private String description;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User userId;
+}
